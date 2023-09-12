0fd73afdc44c5f1014195c72d77083a9e19d70a9
Nathan Tavares <nathantavaresr@gmail.com>
2023-09-12 18:58:46 -0300

    a

--- app/src/main/java/com/example/calculadoracelular/calculoIMC.java
+++ app/src/main/java/com/example/calculadoracelular/calculoIMC.java
@@ -2,7 +2,9 @@ package com.example.calculadoracelular;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,5 +34,28 @@ public class calculoIMC extends AppCompatActivity {
 
     public void mostrarResultado(){
 
+        if (TextUtils.isEmpty(nome.getText().toString())){
+            nome.setError("Campo Obrigatório!");
+            return;
+        }
+        if (TextUtils.isEmpty(idade.getText().toString())){
+            nome.setError("Campo Obrigatório!");
+            return;
+        }
+        if (TextUtils.isEmpty(peso.getText().toString())){
+            peso.setError("Campo Obrigatório!");
+            return;
+        }
+        if (TextUtils.isEmpty(altura.getText().toString())){
+            altura.setError("Campo Obrigatório!");
+            return;
+        }
+
+        Intent intent = new Intent(this, resultadoIMC.class);
+        intent.putExtra("nome",nome.getText().toString());
+        intent.putExtra("idade",idade.getText().toString());
+        intent.putExtra("altura",altura.getText().toString());
+        intent.putExtra("peso",peso.getText().toString());
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/calculadoracelular/resultadoIMC.java
+++ app/src/main/java/com/example/calculadoracelular/resultadoIMC.java
@@ -2,13 +2,28 @@ package com.example.calculadoracelular;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class resultadoIMC extends AppCompatActivity {
-
+    TextView resultado;
+    String strNome;
+    Float fltIdade, fltPeso, fltAltura, fltResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_imc);
+
+        resultado = (TextView) findViewById(R.id.textViewResultado);
+
+        Intent intent = getIntent();
+        strNome = intent.getStringExtra("nome");
+        fltIdade = Float.parseFloat(intent.getStringExtra("idade"));
+        fltAltura = Float.parseFloat(intent.getStringExtra("altura"));
+        fltPeso = Float.parseFloat(intent.getStringExtra("peso"));
+        fltResultado = fltPeso / (fltAltura * fltAltura);
+
+        resultado.setText(fltResultado.toString());
     }
 }
