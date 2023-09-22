b4c78e1e21bf530eeea8ee70a7a498645a471350
Nathan Tavares <nathantavaresr@gmail.com>
2023-09-22 20:31:43 -0300

    a

--- app/src/main/java/com/example/calculadoracelular/bhaskara.java
+++ app/src/main/java/com/example/calculadoracelular/bhaskara.java
@@ -1,25 +1,20 @@
 package com.example.calculadoracelular;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class bhaskara extends AppCompatActivity {
-
-    EditText ValorA, ValorB, ValorC;
-
-    Button btnCalcularBhaskara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bhaskara);
 
-        ValorA = (EditText) findViewById(R.id.editTextValorA);
-        ValorB = (EditText) findViewById(R.id.editTextValorB);
-        ValorC = (EditText) findViewById(R.id.editTextValorC);
-        btnCalcularBhaskara = (Button) findViewById(R.id.btnCalcularBhaskara);
     }
+
 }
